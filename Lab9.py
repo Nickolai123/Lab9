d1e28a9e9d2913d3c4b3ed18c4b13ae50e9d5f63
brian-rumsey <brian.rumsey@ufl.edu>
2024-04-10 10:29:24 -0400

    Brian's changes

--- Lab9.py
+++ Lab9.py
@@ -5,3 +5,12 @@ def encoder(password_input):
         else:
             num -= 7
     return password_input
+
+
+def decoder(password_input):
+    for num in password_input:
+        if num >= 3:
+            num -= 3
+        else:
+            num += 7
+    return password_input
